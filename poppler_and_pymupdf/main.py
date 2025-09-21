5e7534b8d2fa38763c5bb6ee338088daee6534e3
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-09-21 23:12:27 +0200

    feat(main.py): added in poppler pdftotext step

--- poppler_and_pymupdf/main.py
+++ poppler_and_pymupdf/main.py
@@ -10,28 +10,43 @@ from pathlib import Path
 
 import pymupdf
 
+from add_headings import add_headings
 from extract_headings_from_toc_v3 import extract_headings_from_toc, TableOfContentsEntry
 from pdftotext_async import pdftotext_async
 
 
-def main():
+def text_from_pdf():
+    """
+    TODO.
+    """
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-i", "--input", type=Path, help="path to PDF")
     arg_parser.add_argument("-o", "--output", type=Path, help="desired output path")
     args = arg_parser.parse_args()
 
     doc = pymupdf.open(args.input)
+    print("extracting items from Table of Contents")
     toc: list[TableOfContentsEntry] = extract_headings_from_toc(doc)
     with open(args.input, "rb") as file:
         doc_content: bytes = file.read()
 
-    doc_text: str = asyncio.run(
+    print("extracting PDF text")
+    doc_text: list[str] = asyncio.run(
         pdftotext_async(doc_content),
     )
 
-    with open(args.output, "w") as file:
-        file.write(doc_text)
+    print("Annotating headings in document")
+    doc_text_with_headings, headings_report = add_headings(
+        doc_text=doc_text,
+        toc=toc,
+    )
+
+    print("Exporting results")
+    with open(args.output, "w", encoding="utf-8") as file:
+        file.write(doc_text_with_headings)
+
+    return headings_report
 
 
 if __name__ == "__main__":
-    main()
+    process_report: dict = text_from_pdf()
